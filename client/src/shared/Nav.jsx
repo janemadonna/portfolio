be8ab5937ec3b9f8ebe269e0b3f6e654c0da1be2
Jane Madonna <janecogger@gmail.com>
2020-01-06 16:14:48 -0500

    added more styling

--- client/src/shared/Nav.jsx
+++ client/src/shared/Nav.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import burger from '../images/menu-24px.svg'
 
-const Nav = () => {
-    return (
-        <div className='nav-bar'>
-            <NavLink className='nav-link' to='/'>Home</NavLink>
-            <NavLink className='nav-link' to='/about'>About</NavLink>
-            <NavLink className='nav-link' to='/projects'>Projects</NavLink>
-            <NavLink className='nav-link' to='/contact'>Contact</NavLink>
-        </div>
-    )
+class Nav extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            display: 'active'
+        }
+    }
+
+    handleclick = () => {
+        this.setState({display: 'inactive'})
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='nav-bar'>
+                    <NavLink className='nav-link home-link' to='/'>HOME</NavLink>
+                    <div className='drop-down'>
+                        <div>
+                            <img className='burger' onClick={this.handleClick} alt='burger menu' src={burger}/>
+                        </div>
+                    </div>
+                </div>
+                <div className={this.state.display}>
+                    <NavLink className='nav-link' to='/about'>ABOUT</NavLink>
+                    <NavLink className='nav-link' to='/projects'>PROJECTS</NavLink>
+                    <NavLink className='nav-link' to='/contact'>CONTACT</NavLink>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Nav

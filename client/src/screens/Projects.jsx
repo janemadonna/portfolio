f678a1620df518c2bbc923e690bded735e7b6900
Jane Madonna <janecogger@gmail.com>
2020-01-22 08:47:09 -0800

    started projects page

--- client/src/screens/Projects.jsx
+++ client/src/screens/Projects.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import coinfox from '../images/coinfox.gif'
 
 const Projects = () => {
     return (
         <div className='projects'>
-            Projects
+            <div className='worder'>
+                <a href='https://worder.surge.sh'>wOrder</a>
+            </div>
+            <div className='coinfox'>
+                <h4>COINFOX</h4>
+                <p>A fantasy cryptocurrency portfolio database made with CRUD in React, using a third-party API and a mock API.</p>
+                <img className='coinfox-gif' src={coinfox} alt='coinfox gif' />
+                <a href='https://coinfox.surge.sh'><p className='project-link'>Visit Coinfox</p></a>
+            </div>
+            <div className='astral-plan'>
+                <a href='https://astral-plan.surge.sh'>Astral Plan</a>
+            </div>
         </div>
     )
 }

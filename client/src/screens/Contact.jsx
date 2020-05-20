ffd3f31df52b3d4d889d8e8a71ba252b43fda1ac
janemadonna <janecogger@gmail.com>
2020-05-20 13:24:50 -0400

    fixed contact page

--- client/src/screens/Contact.jsx
+++ client/src/screens/Contact.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import avatar from '../images/work-avatar.png'
 
 const Contact = () => {
     return (
         <div className='contact'>
             <p>janecogger@gmail.com</p>
+            <img src={avatar} />
         </div>
     )
 }

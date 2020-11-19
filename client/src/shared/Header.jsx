90c49c591d68e5494c54d74657776825dd87c789
janemadonna <janecogger@gmail.com>
2020-11-18 18:44:04 -0800

    shuffling and name animation

--- client/src/shared/Header.jsx
+++ client/src/shared/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
         <div className='nav-header'>
             <header>
                 <NavLink className='name' to='/'><h1>JANE COGGER</h1></NavLink>
+                <NavLink className='name2' to='/'><h1>JANE COGGER</h1></NavLink>
             </header>
             <div className='nav-links'>
                 <NavLink className='nav-link' to='/about'>ABOUT</NavLink>
